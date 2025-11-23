81974d89e2c6aa28ee70dce681961c6b42bc9e6a
Venosta-web <popeepopepope@gmail.com>
2025-11-23 12:01:27 +0100

    readdingrollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import css from 'rollup-plugin-css-only';
+
+export default {
+  input: 'src/growspace-manager-card.ts',
+  output: {
+    file: 'dist/growspace-manager-card.js',
+    format: 'es',
+    inlineDynamicImports: true,
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
+    terser(),
+    css({ output: false })
+  ],
+};
